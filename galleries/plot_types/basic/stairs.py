ac7bef25894c9f49822b58e137bb800eb5d42a46
SPANDAN PADHI <123059161+spandan0724@users.noreply.github.com>
2023-10-28 16:01:37 +0530

    DOC: Direct to the entry regarding the 'step' function within the 'stairs' plot type, identified by #27194.

--- galleries/plot_types/basic/stairs.py
+++ galleries/plot_types/basic/stairs.py
@@ -3,17 +3,17 @@
 stairs(values)
 ==============
 
-See `~matplotlib.axes.Axes.stairs`.
+Visualizes changes between values at x[i] and x[i+1] using `~matplotlib.axes.Axes.stairs`.
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('_mpl-gallery')
 
-# make data
+# Make data
 y = [4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
 
-# plot
+# Plot
 fig, ax = plt.subplots()
 
 ax.stairs(y, linewidth=2.5)
